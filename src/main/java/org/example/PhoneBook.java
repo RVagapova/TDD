482b27cfa0c7c3d418f2e02ae5088878dc37c096
Regina <regina.vagapova95@gmail.com>
2023-12-17 17:16:55 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import java.util.Map;
+
 public class PhoneBook {
+
+    public static int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,21 @@
+import org.example.PhoneBook;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        int expected1 = 1;
+        int expected2 = 2;
+        int expected3 = 2;
+
+        Assert.assertEquals(expected1, PhoneBook.add("Александр", "+74957480110"));
+        Assert.assertEquals(expected2, PhoneBook.add("Павел", "89613795611"));
+        Assert.assertEquals(expected3, PhoneBook.add("Алекснадр", "+79871234569"));
+    }
 }
